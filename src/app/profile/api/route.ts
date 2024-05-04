ba77147a949982549b9162f2c4ee77ed7a41c8c4
Sanjay <sanjaykeloth321@gmail.com>
2024-05-05 00:09:59 +0530

    Implemented cookies in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   // const requestHeaders = new Headers(request.headers);
@@ -8,9 +8,16 @@ export async function GET(request: NextRequest) {
   const headerList = headers();
   console.log(headerList.get("Authorization"));
 
+  // const theme = request.cookies.get("theme");
+  // console.log(theme);
+
+  cookies().set("resultsPerPage", "20");
+  console.log(cookies().get("resultsPerPage"));
+
   return new Response("<h1>Profile API data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
