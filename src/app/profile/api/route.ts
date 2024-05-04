91f68fadf82654b31acd604b332effb28182379e
Sanjay <sanjaykeloth321@gmail.com>
2024-05-04 23:57:10 +0530

    Implemented headers in route handlers

---
+++ src/app/profile/api/route.ts
@@ -0,0 +1,16 @@
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  // const requestHeaders = new Headers(request.headers);
+  // console.log(requestHeaders.get("Authorization"));
+
+  const headerList = headers();
+  console.log(headerList.get("Authorization"));
+
+  return new Response("<h1>Profile API data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
+}
