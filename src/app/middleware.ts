c089bbc10291bf5161b12e9af8df153558375670
Sanjay <sanjaykeloth321@gmail.com>
2024-05-05 10:36:33 +0530

    Implemented middlewares in route handlers

---
+++ src/app/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  const themePreference = request.cookies.get("theme");
+  if (!themePreference) {
+    response.cookies.set("theme", "dark");
+  }
+
+  response.headers.set("custom-header", "custom-value");
+
+  //   return NextResponse.redirect(new URL("/", request.url));
+  //   if (request.nextUrl.pathname === "/profile/api") {
+  //     return NextResponse.rewrite(new URL("/hello", request.url));
+  //   }
+}
+
+// export const config = {
+//   matcher: "/profile/api",
+// };
